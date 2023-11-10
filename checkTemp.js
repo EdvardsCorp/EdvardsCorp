599ea2f97cb92e82d35cad8ecb9fa12ebf6bd729
Edvard <140340191+EdvardsCorp@users.noreply.github.com>
2023-11-10 10:34:59 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,19 +1,18 @@
-temp = 30; // <- prompt("Sisesta temperatuur")
-cel = parseInt (temp); // cel = mis sisestub temp-ist / näide 19
+Temp = 40;
 
-function checkTemp() {
+function checkTemp(Temp) {
   // kui sisetatud nr väiksem/võrdne kui 20 käivita funkt // kui nr on suurem kui 20 liigu edasi
-  if (cel <= 20) {
-    return "-1"; // funkt utleb teha : näita con.log ("-1")
+  if (Temp <= 20) {
+    return -1; // funkt utleb teha : näita con.log ("-1")
   }
   // või siis kui sisestatud nr väiksem võrdne 40 kui nr on suurem kui 40 liigu edasi
-  else if (cel <= 40) {
-    return "0"; // siis näita con.log ("0")
-  } else if (cel >= 41) {
-    return "1";
-  } else if (cel != "") {
+  else if (Temp <= 40) {
+    return 0; // siis näita con.log ("0")
+  } else if (Temp >= 41) {
+    return 1;
+  } else if (Temp != "") {
     return "Sisesta number!";
   }
 }
 
-console.log(checkTemp()); 
+console.log(checkTemp(Temp));
